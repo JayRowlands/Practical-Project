230dea17ffc25382b3efcd9a900a72289541a9f8
JayRowlands <56541131+JayRowlands@users.noreply.github.com>
2021-12-02 12:27:59 +0000

    working routes

--- front-end/application/routes.py
+++ front-end/application/routes.py
@@ -5,5 +5,9 @@ import requests
 
 @app.route('/', methods = ['GET'])
 def home():
-
-    return render_template('index.html')
+    
+    pickup_location = requests.get('http://service2-api:5001/get_location')
+    grabber_resistance = requests.get('http://service3-api:5002/get_resistance')
+    prize= requests.post('http://service4-api:5003/get_prize', json= {"location" : pickup_location.text, "resistance" : grabber_resistance.text})
+    
+    return render_template('index.html', pickup_location=pickup_location.text, grabber_resistance=grabber_resistance.text, prize=prize.text)
